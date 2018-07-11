a0f32f543c9e367df2cec0f6bbcbb517ccfb8967
Angelina Blyth <angelinalblyth@hotmail.com>
2018-07-11 14:08:37 +0100

    changed main contoller

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -7,9 +7,9 @@ public class Seeds {
 
     public static void seedData(){
 
-        Borrower borrower1 = new Borrower("Adri");
-        Borrower borrower2 = new Borrower("Angelina");
-        Borrower borrower3 = new Borrower("Harjit");
+        Borrower borrower1 = new Borrower("Adri", "Florence");
+        Borrower borrower2 = new Borrower("Angelina", "Blyth");
+        Borrower borrower3 = new Borrower("Harjit", "Singh");
         Book book1 = new Book("Do Androids Dream Of Electric Sheep?", "Philip K. Dick");
         Book book2 = new Book("Harry Potter and the Goblet of Fire", "J. K. Rowling");
         Book book3 = new Book("Down and Out in Paris and London", "George Orwell");
