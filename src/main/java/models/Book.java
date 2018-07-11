d22e9f904a58f61ea88758f7a06c047d4d3c0b6d
Angelina Blyth <angelinalblyth@hotmail.com>
2018-07-11 12:17:50 +0100

    Book class finished. Need to add relationship between borrowers

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,7 +1,70 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="books")
 public class Book {
 
     private int id;
     private String title;
+    private String author;
+    private Boolean onLoan;
+    private Borrower currentBorrower;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, Boolean onLoan, Borrower currentBorrower) {
+        this.title = title;
+        this.author = author;
+        this.onLoan = onLoan;
+        this.currentBorrower = currentBorrower;
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "author")
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Column(name="onLoan")
+    public Boolean getOnLoan() {
+        return onLoan;
+    }
+
+    public void setOnLoan(Boolean onLoan) {
+        this.onLoan = onLoan;
+    }
+
+    public Borrower getCurrentBorrower() {
+        return currentBorrower;
+    }
+
+    public void setCurrentBorrower(Borrower currentBorrower) {
+        this.currentBorrower = currentBorrower;
+    }
 }
