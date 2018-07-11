62988af045cfdc9c5019ce006452dadf74463e51
Angelina Blyth <angelinalblyth@hotmail.com>
2018-07-11 13:00:37 +0100

    Added library class

---
+++ src/main/java/models/Library.java
@@ -0,0 +1,33 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book> books;
+    private ArrayList<Borrower> borrowers;
+
+    public Library() {
+    }
+
+    public Library() {
+        this.books = new ArrayList<Book>();
+        this.borrowers = new ArrayList<Borrower>();
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public ArrayList<Borrower> getBorrowers() {
+        return borrowers;
+    }
+
+    public void setBorrowers(ArrayList<Borrower> borrowers) {
+        this.borrowers = borrowers;
+    }
+}
