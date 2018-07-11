24be341bf0552dbfc781685075c806cf9f30c151
Adri <forczekadri@gmail.com>
2018-07-11 12:20:11 +0100

    adds borrower class

---
+++ src/main/java/models/Borrower.java
@@ -0,0 +1,45 @@
+package models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "borrowers")
+public class Borrower {
+
+    private int id;
+    private String name;
+    private Set<Book> itemsBorrowed;
+
+    public Borrower(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy="borrower", fetch = FetchType.LAZY)
+    public Set<Book> getItemsBorrowed() {
+        return itemsBorrowed;
+    }
+
+    public void setItemsBorrowed(Set<Book> itemsBorrowed) {
+        this.itemsBorrowed = itemsBorrowed;
+    }
+}
